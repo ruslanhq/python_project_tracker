dce7b0cad0ee73328912606e042e41f7d2824c3d
Ruslan <h.ruslan97@gmail.com>
2019-09-26 14:15:13 +0600

    Added IndexView

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
+from webapp.models import Task
 # Create your views here.
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all()
+        return context
+
+
+
+
