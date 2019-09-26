658ce2bcb2062d93ce3031003ffabe96101140f0
Ruslan <h.ruslan97@gmail.com>
2019-09-26 13:05:10 +0600

    Added models: Task, Type, Status. + migrations and fixtures

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from webapp.models import Task, Status, Type
+
+
+# class TaskAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'title', 'author', 'created_at']
+#     list_filter = ['author']
+#     search_fields = ['title', 'text']
+#     fields = ['title', 'author', 'text', 'created_at', 'updated_at']
+#     readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Task)
+admin.site.register(Status)
+admin.site.register(Type)
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    summary = models.CharField(max_length=40, verbose_name='Заголовок')
+    description = models.TextField(max_length=400, null=True, blank=True, verbose_name='Полное описание')
+    status = models.ForeignKey('webapp.Status', related_name='statuses', on_delete=models.PROTECT, verbose_name='Статус')
+    type = models.ForeignKey('webapp.Type', related_name='types', on_delete=models.PROTECT, verbose_name='Тип')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.summary
+
+
+class Status(models.Model):
+    status = models.CharField(max_length=10, verbose_name='Статус')
+
+    def __str__(self):
+        return self.status
+
+
+class Type(models.Model):
+    type = models.CharField(max_length=10, verbose_name='Тип')
+
+    def __str__(self):
+        return self.type
+
