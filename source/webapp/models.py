377d7846c565e729d70cc864b5b46aab3dda77c4
Ruslan <h.ruslan97@gmail.com>
2019-10-10 17:00:38 +0600

    Added foreignkey, and migrations

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -8,6 +8,8 @@ class Task(models.Model):
     description = models.TextField(max_length=400, null=True, blank=True, verbose_name='Полное описание')
     status = models.ForeignKey('webapp.Status', related_name='statuses', on_delete=models.PROTECT, verbose_name='Статус')
     type = models.ForeignKey('webapp.Type', related_name='types', on_delete=models.PROTECT, verbose_name='Тип')
+    project = models.ForeignKey('webapp.Project', related_name='projects', on_delete=models.PROTECT, null=True,
+                                blank=False, verbose_name='Проект')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
 
     def __str__(self):
@@ -33,5 +35,9 @@ class Project(models.Model):
     description = models.TextField(max_length=400, null=True, blank=True, verbose_name='Описание проекта')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата обновления')
+
+    def __str__(self):
+        return self.name
+
 
 
