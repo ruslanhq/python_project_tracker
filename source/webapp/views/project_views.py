22ab602d4c34b8f4d1ae50b34935514e15829888
Ruslan <h.ruslan97@gmail.com>
2019-10-14 16:52:43 +0600

    Added search for Project

--- source/webapp/views/project_views.py
+++ source/webapp/views/project_views.py
@@ -1,7 +1,9 @@
+from django.db.models import Q
 from django.urls import reverse, reverse_lazy
+from django.utils.http import urlencode
 from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from webapp.forms import ProjectForm
+from webapp.forms import ProjectForm, SimpleSearchForm
 from webapp.models import Project
 
 
@@ -12,6 +14,33 @@ class ProjectList(ListView):
     ordering = '-created_at'
     paginate_by = 5
     paginate_orphans = 1
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_query = self.get_search_query()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        if self.search_query:
+            context['query'] = urlencode({'search': self.search_query})
+        context['form'] = self.form
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=self.search_query))
+        return queryset
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_query(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 
 class ProjectView(DetailView):
