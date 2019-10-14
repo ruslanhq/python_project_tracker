1dbd141a1c10fdc080bbde47be86f80ad4118345
Ruslan <h.ruslan97@gmail.com>
2019-10-14 16:43:39 +0600

    Added SearchForm in forms.py and search in task_views

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -4,20 +4,10 @@ from django.forms import widgets
 from webapp.models import Status, Type, Task, Project
 
 
-# class TaskForm(forms.Form):
-#     summary = forms.CharField(max_length=40, required=True, label='Заголовок')
-#     description = forms.CharField(max_length=400, required=False, label='Описание', widget=widgets.Textarea)
-#     status = forms.ModelChoiceField(queryset=Status.objects.all(), required=False, label='Статус', empty_label=None)
-#     type = forms.ModelChoiceField(queryset=Type.objects.all(), required=False, label='Тип', empty_label=None)
-
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         exclude = ['created_at']
-
-#
-# class StatusForm(forms.Form):
-#     status = forms.CharField(max_length=10, required=True,  label='Название')
 
 
 class StatusForm(forms.ModelForm):
@@ -36,3 +26,7 @@ class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
         exclude = ['updated_at']
+
+
+class SimpleSearchForm(forms.Form):
+    search = forms.CharField(max_length=100, required=False, label="Найти")
--- source/webapp/views/task_views.py
+++ source/webapp/views/task_views.py
@@ -1,8 +1,10 @@
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse, reverse_lazy
+from django.utils.http import urlencode
 from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
 
-from webapp.forms import TaskForm
+from webapp.forms import TaskForm, SimpleSearchForm
 from webapp.models import Task
 # from .base_views import DetailView, UpdateView
 # Create your views here.
@@ -15,6 +17,35 @@ class IndexView(ListView):
     ordering = '-created_at'
     paginate_by = 2
     paginate_orphans = 1
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_query = self.get_search_query()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        if self.search_query:
+            context['query'] = urlencode({'search': self.search_query})
+        context['form'] = self.form
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_query:
+            queryset = queryset.filter(
+                Q(summary__icontains=self.search_query)
+                | Q(description__icontains=self.search_query)
+            )
+        return queryset
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_query(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 
 class TaskView(DetailView):
