77ced241752ebd1eb78e64153b4f6543b0c1da5d
Ruslan <h.ruslan97@gmail.com>
2019-10-24 16:20:20 +0600

    Added new app accounts

--- source/main/settings.py
+++ source/main/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'widget_tweaks',
     'webapp',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'accounts:login'
+
