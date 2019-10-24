1d5969f0ca4e5d4a70ecd10c1d23a486340db463
Ruslan <h.ruslan97@gmail.com>
2019-10-24 16:54:02 +0600

    Added bonus

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -5,13 +5,15 @@ from django.shortcuts import render, redirect
 
 def login_view(request):
     context = {}
+    next = request.GET.get('next')
+    redirect_page = request.session.setdefault('redirect_page', next)
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('index')
+            return redirect(redirect_page)
         else:
             context['has_error'] = True
     return render(request, 'login.html', context=context)
