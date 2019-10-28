380829e993ebac77f2238163ca8635f07a5b37c0
Ruslan <h.ruslan97@gmail.com>
2019-10-28 15:10:17 +0600

    Added register_view and user create form and validation

---
+++ source/accounts/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+class UserCreationForm(forms.Form):
+    username = forms.CharField(max_length=100, label='Username', required=True)
+    password = forms.CharField(max_length=100, label='Password', required=True,
+                               widget=forms.PasswordInput)
+    password_confirm = forms.CharField(max_length=100, label='Password Confirm', required=True,
+                                       widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        try:
+            User.objects.get(username=username)
+            raise ValidationError('User with this username already exists',
+                                  code='username_exists')
+        except User.DoesNotExist:
+            return username
+
+    def clean(self):
+        super().clean()
+        password_1 = self.cleaned_data['password']
+        password_2 = self.cleaned_data['password_confirm']
+        if password_1 != password_2:
+            raise ValidationError('Passwords do not match', code='passwords_do_not_match')
+        return self.cleaned_data
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,19 +1,20 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+from accounts.forms import UserCreationForm
 
 
 def login_view(request):
     context = {}
-    next = request.GET.get('next')
-    redirect_page = request.session.setdefault('redirect_page', next)
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect(redirect_page)
+            return redirect('index')
         else:
             context['has_error'] = True
     return render(request, 'login.html', context=context)
@@ -23,3 +24,20 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('index')
+
+
+def register_view(request):
+    if request.method == 'GET':
+        form = UserCreationForm()
+        return render(request, 'register.html', {'form': form})
+    elif request.method == 'POST':
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = User(username=form.cleaned_data['username'])
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'register.html', {'form': form})
+
