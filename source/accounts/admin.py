e80c57ac39c49493bba00a4a174cf99c6b9a9db6
Ruslan <h.ruslan97@gmail.com>
2019-11-03 21:32:51 +0600

    Register in admin.py

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import Profile
+from django.contrib.auth.admin import UserAdmin
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    fields = ['birth_date', 'avatar']
+
+
+class UserProfileAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserProfileAdmin)
+
