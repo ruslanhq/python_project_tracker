efa76b801ffc2b871778d3e66bf9e4b7e0407164
Ruslan <h.ruslan97@gmail.com>
2019-11-04 14:40:51 +0600

    Added in model Profile "about_me" and "git"

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -6,6 +6,8 @@ class Profile(models.Model):
     user = models.OneToOneField(User, related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
     birth_date = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
     avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
+    about_me = models.TextField(null=True, blank=True, max_length=200, verbose_name='О себе')
+    git = models.URLField(verbose_name='Профиль github', null=True, blank=True)
 
     def __str__(self):
         return self.user.get_full_name() + "'s Profile"
