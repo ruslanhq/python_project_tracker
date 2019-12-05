f9c404c7439cdda9fd11555c34bc140fc3a657ba
Ruslan <h.ruslan97@gmail.com>
2019-12-05 17:41:44 +0600

    Done Project

--- source/main/urls.py
+++ source/main/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('webapp.urls'))
+    path('', include('webapp.urls')),
+    path('api/', include('api.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
